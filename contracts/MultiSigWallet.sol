af01f5f282912c81fb674d259e9077320969bba8
cenwadike <chinedunwadike04@gmail.com>
2024-08-05 08:12:44 +0100

    first commit

---
+++ contracts/MultiSigWallet.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint indexed txId);
+    event Approve(address indexed owner, uint indexed txId);
+    event Revoke(address indexed owner, uint indexed txId);
+    event Execute(uint indexed txId);
+
+    struct Transaction {
+        address destination;
+        uint value;
+        bytes data;
+        bool executed;
+    }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+
+    uint public requiredApprovals;
+
+    Transaction[] public transactions;
+    mapping(uint => mapping (address => bool)) public approved;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "Unauthorized: not owner");
+        _;
+    }
+
+    modifier txExist(uint _txId) {
+        require(_txId < transactions.length, "tx does not exists");
+        _;
+    }
+
+    modifier notApproved(uint _txId) {
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
+
+    constructor(address[] memory _owners, uint _requiredApprovals) {
+        require(_owners.length > 0, "Owners required");
+        require(_requiredApprovals > 0 && _requiredApprovals <= _owners.length, 
+            "Invalid required number of approvals"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "Invalid address");
+            require(!isOwner[owner], "Owners must be unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        requiredApprovals = _requiredApprovals;
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function submit(address _destination, uint _value, bytes calldata _data) external onlyOwner {
+        transactions.push(Transaction({
+            destination: _destination,
+            value: _value,
+            data: _data,
+            executed: false
+        }));
+
+        emit Submit(transactions.length - 1);
+    }
+
+    function approve(uint _txId) external onlyOwner txExist(_txId) notApproved(_txId) notExecuted(_txId) {
+        approved[_txId][msg.sender] = true;
+
+        emit Approve(msg.sender, _txId);
+    }
+
+    function revoke(uint _txId) external onlyOwner txExist(_txId) notExecuted(_txId) {
+        require(approved[_txId][msg.sender], "tx not approved");
+
+        approved[_txId][msg.sender] = false;
+
+        emit Revoke(msg.sender, _txId);
+    }
+
+    function execute(uint _txId) payable external txExist(_txId) notExecuted(_txId) {
+        require(_getApprovalCount(_txId) >= requiredApprovals, "Approvals is less than required");
+        Transaction storage transaction = transactions[_txId];
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.destination.call{value: transaction.value}(transaction.data);
+
+        require(success, "tx failed");
+
+        emit Execute(_txId);
+    }
+
+    function getTransactionIndex() external view onlyOwner returns (uint) {
+        uint index = transactions.length;
+
+        if(index > 0) {
+            return index - 1;
+        }else {
+            return 0;
+        }
+    }
+
+     function getTransactionDetails(
+        uint _txId
+    )
+        public
+        view
+        returns (
+            address destinantion,
+            uint value,
+            bytes memory data,
+            bool executed
+        )
+    {
+        Transaction memory transaction = transactions[_txId];
+        return (
+            transaction.destination,
+            transaction.value,
+            transaction.data,
+            transaction.executed
+        );
+    }
+
+    function _getApprovalCount(uint _txId) private view returns (uint count) {
+        for (uint i = 0; i < owners.length; i++) {
+            if(approved[_txId][owners[i]]) {
+                count++;
+            }
+        }
+    }
+}
+
---
+++ test/MultiSigWallet.js
@@ -0,0 +1,157 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MultiSigWallet", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployMultiSigWalletFixture() {
+    const minApprovals = 2;
+
+    // Contracts are deployed using the first signer/account by default
+    const [owner1, owner2, owner3] = await ethers.getSigners();
+
+    const MultiSigWallet = await ethers.getContractFactory("MultiSigWallet");
+    const multiSigWallet = await MultiSigWallet.deploy([owner1, owner2, owner3], minApprovals);
+
+    const contractAddress = await multiSigWallet.getAddress();
+
+    return { multiSigWallet, minApprovals, owner1, owner2, owner3, contractAddress };
+  }
+
+  describe("Deployment", function () {
+    it("Should deploy contract", async function () {
+      await loadFixture(deployMultiSigWalletFixture);
+    });
+  });
+
+  describe("Multiple signature", function () {
+    describe("Validations", function () {
+      it("Should fail to submit transaction", async function () {
+        const { multiSigWallet, contractAddress, } = await loadFixture(deployMultiSigWalletFixture);
+        const [_1, _2, _3, notOwner] = await ethers.getSigners();
+
+        await expect(multiSigWallet.connect(notOwner).submit(contractAddress, 1, ethers.randomBytes(32))).to.be.revertedWith(
+          "Unauthorized: not owner"
+        );
+      });
+
+      it("Should fail to approve transaction", async function () {
+        const { multiSigWallet } = await loadFixture(deployMultiSigWalletFixture);
+        const [_1, _2, _3, notOwner] = await ethers.getSigners();
+
+        await expect(multiSigWallet.connect(notOwner).approve(0)).to.be.revertedWith(
+          "Unauthorized: not owner"
+        );
+      });
+
+      it("Should fail to revoke transaction", async function () {
+        const { multiSigWallet } = await loadFixture(deployMultiSigWalletFixture);
+        const [_1, _2, _3, notOwner] = await ethers.getSigners();
+
+        await expect(multiSigWallet.connect(notOwner).revoke(0)).to.be.revertedWith(
+          "Unauthorized: not owner"
+        );
+      });
+
+      it("Should fail to execute transaction", async function () {
+        const { multiSigWallet } = await loadFixture(deployMultiSigWalletFixture);
+        const [_1, _2, _3, notOwner] = await ethers.getSigners();
+
+        await expect(multiSigWallet.connect(notOwner).execute(0)).to.be.revertedWith(
+          "tx does not exists"
+        );
+      });
+    });
+
+    describe("Simulation", function () {
+      it("Should submit a transaction", async function () {
+        const { multiSigWallet, owner1, contractAddress } = await loadFixture(deployMultiSigWalletFixture);
+
+        await expect(multiSigWallet.connect(owner1).submit(contractAddress, 1, ethers.randomBytes(32)))
+          .to.emit(multiSigWallet, "Submit").withArgs(0);
+      });
+
+      it("Should submit multiple transactions", async function () {
+        const { multiSigWallet, owner1, owner2, owner3, contractAddress } = await loadFixture(deployMultiSigWalletFixture);
+
+        await expect(multiSigWallet.connect(owner1).submit(contractAddress, 1, ethers.randomBytes(32)))
+          .to.emit(multiSigWallet, "Submit").withArgs(0);
+
+        await expect(multiSigWallet.connect(owner2).submit(contractAddress, 1, ethers.randomBytes(32)))
+          .to.emit(multiSigWallet, "Submit").withArgs(1);
+
+        await expect(multiSigWallet.connect(owner3).submit(contractAddress, 1, ethers.randomBytes(32)))
+        .to.emit(multiSigWallet, "Submit").withArgs(2);
+
+      });
+
+      it("Should submit and approve transaction", async function () {
+        const { multiSigWallet, owner1, contractAddress } = await loadFixture(deployMultiSigWalletFixture);
+
+        // submit transaction
+        multiSigWallet.connect(owner1).submit(contractAddress, 1, ethers.randomBytes(32));
+
+        // get transaction index
+        const index = await multiSigWallet.getTransactionIndex();
+
+        await expect(multiSigWallet.connect(owner1).approve(index)).to.emit(multiSigWallet, "Approve").withArgs(owner1.address, index);
+      });
+
+      it("Should submit transaction, approve transaction and revoke transaction", async function () {
+        const { multiSigWallet, owner1, contractAddress } = await loadFixture(deployMultiSigWalletFixture);
+
+        // submit transaction
+        multiSigWallet.connect(owner1).submit(contractAddress, 1, ethers.randomBytes(32));
+
+        // get transaction index
+        const index = await multiSigWallet.getTransactionIndex();
+
+        // approve transaction
+        await multiSigWallet.connect(owner1).approve(index)
+      
+        // revoke approval
+        await expect(multiSigWallet.connect(owner1).revoke(index)).to.emit(multiSigWallet, "Revoke").withArgs(owner1.address, index);
+      });
+
+      it("Should submit transaction and multiple approval on transaction", async function () {
+        const { multiSigWallet, owner1, owner2, owner3, contractAddress } = await loadFixture(deployMultiSigWalletFixture);
+
+        // submit transaction
+        multiSigWallet.connect(owner1).submit(contractAddress, ethers.parseEther("0.001"), ethers.toUtf8Bytes("blob"));
+
+        // get transaction index
+        const index = await multiSigWallet.getTransactionIndex();
+
+        await expect(multiSigWallet.connect(owner1).approve(index)).to.emit(multiSigWallet, "Approve").withArgs(owner1.address, index);
+        await expect(multiSigWallet.connect(owner2).approve(index)).to.emit(multiSigWallet, "Approve").withArgs(owner2.address, index);
+        await expect(multiSigWallet.connect(owner3).approve(index)).to.emit(multiSigWallet, "Approve").withArgs(owner3.address, index);
+      });
+
+      it("Should submit multiple approval and execute transaction", async function () {
+        const { multiSigWallet, owner1, owner2, owner3, contractAddress } = await loadFixture(deployMultiSigWalletFixture);
+
+        // submit transaction
+        multiSigWallet.connect(owner1).submit(ethers.getAddress(contractAddress), ethers.parseEther("0.1"), ethers.randomBytes(32));
+
+        // get transaction index
+        const index = await multiSigWallet.getTransactionIndex();
+
+        // approve transaction
+        await multiSigWallet.connect(owner1).approve(index);
+        await multiSigWallet.connect(owner2).approve(index);
+        await multiSigWallet.connect(owner3).approve(index);
+
+        // execute transaction
+        await expect(multiSigWallet.connect(owner3).execute(index)).to.be.revertedWith(
+          "tx failed"
+        );
+      });
+    });
+  });
+});
